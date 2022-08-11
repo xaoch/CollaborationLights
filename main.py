a67be9bdd39957d5888ecf49cc463e9a325ae020
Xavier Ochoa <xavier.ochoa@gmail.com>
2022-08-11 15:39:18 -0400

    Adding Logic

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def initStudents():
 
         for i in range(0,numberStudents):
                 studentStatus[i]= "Middle"
-                studentTime[i]= 600
+                studentTime[i]= 60
                 studentRecentTime[i]=300
                 studentSpeaking[i]=0
 
